b1962988ab280a6a29aa80d69741723c2de18136
Tyler Lloyd <tyjoll96@gmail.com>
2017-09-18 11:29:34 -0700

    List Dropdown and Styling
    Added dropdown to display different lists.
    
    Styled cards and top bar to resemble Trello.

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
-    //hsp.init({
-        //useTheme: true
-    //});
-    var listSuccess = function (data){
+    var testingSuccess = function (data){
       console.log(data);
-      var messages = $('#messages');
+      getMessages(data[0].id);
+      var lists = $("<select />")
+        .change(function () {
+          console.log ($(this).val());
+          getMessages ($(this).val());
+        });
+
+      data.forEach(function(item) {
+        var item = $('<option />', {value: item.id, text: item.name}).appendTo(lists);
+      });
+
+      lists.appendTo($('#dropdown'));
+      // when list is selected, clear current list of cards and get lists/cards and call list success
+    }
+
+    var testingError = function (data) {
+      console.log(data);
+    }
+    var listSuccess = function (data){
+      //console.log(data);
+      $('#cards').html('');
       data.forEach(function(item, index){
-        console.log(item);
+        //console.log(item);
         var $msg = $("<div>", {"class": "hs_message"});
         var $avatar = $("<div>", {"class": "hs_avatar"});
         $avatar.append('<img class="hs_avatarImage" src="./asset/avatar.png" alt="Avatar">');
@@ -14,11 +31,12 @@ $(document).ready(function() {
         var $content = $("<div>", {"class": "hs_content"});
         $content.append('<a href="#" class="hs_userName" title="Username">'+item.name+'</a>');
         $content.append('<a href="#" class="hs_postTime" target="_blank" title="Sunday, September 14 2014 at 12:00pm via Hootsuite">2 minute ago</a>');
+        
         //var $contentText = $("<div>", {"class": "hs_contentText"}).text(item.name);
         //$content.append($contentText);
         $msg.append($avatar);
         $msg.append($content);
-        messages.append($msg);
+        $('#cards').append($msg);
       });
     }
     var listError = function (){
@@ -27,8 +45,14 @@ $(document).ready(function() {
     var authenticationSuccess = function()
     {
       console.log('Successful authentication');
-      Trello.get("/lists/58dd6f0c1b5d5ea481f146fc/cards", listSuccess, listError)
+      Trello.get("/boards/58dd6ed489ac42e65ededb48/lists", testingSuccess, testingError);
     };
+
+    var getMessages = function (list)
+    {
+      Trello.get("/lists/" + list + "/cards", listSuccess, listError);
+    }
+
     var authenticationFailure = function() {
       console.log('Failed authentication');
     };
